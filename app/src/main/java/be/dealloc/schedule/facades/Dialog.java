ba72763daec17f7f32b6f7005e68945022753502
Wannes Gennar <wannes.gennar@gmail.com>
2016-11-09 11:16:27 +0100

    added input dialog to facade

--- app/src/main/java/be/dealloc/schedule/facades/Dialog.java
+++ app/src/main/java/be/dealloc/schedule/facades/Dialog.java
@@ -4,6 +4,8 @@ package be.dealloc.schedule.facades;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.widget.EditText;
 import be.dealloc.schedule.R;
 
 /**
@@ -39,5 +41,34 @@ public final class Dialog
 	public static AlertDialog msgbox(Context context, final int title, final int content)
 	{
 		return msgbox(context, title, content, null);
+	}
+
+	public static AlertDialog input(Context context, final int title, final int content, final TextDialogCallback onConfirm, DialogInterface.OnClickListener onDeny)
+	{
+		final EditText input = new EditText(context);
+		input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+		DialogInterface.OnClickListener confirm = (onConfirm == null ? null : new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int i)
+			{
+				onConfirm.confirm(dialog, input.getText().toString());
+			}
+		});
+
+		return (new AlertDialog.Builder(context))
+				.setIcon(R.mipmap.ic_launcher)
+				.setTitle(title)
+				.setMessage(content)
+				.setView(input)
+				.setPositiveButton(android.R.string.yes, confirm)
+				.setNegativeButton(android.R.string.no, onDeny)
+				.create();
+	}
+
+	public interface TextDialogCallback
+	{
+		void confirm(DialogInterface dialog, String content);
 	}
 }
