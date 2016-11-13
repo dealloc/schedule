60730de5f4c179d194e035a14f3e32d3791239ef
Wannes Gennar <wannes.gennar@gmail.com>
2016-11-13 18:45:47 +0100

    improved look and feel of course list

--- app/src/main/java/be/dealloc/schedule/adapters/CourseAdapter.java
+++ app/src/main/java/be/dealloc/schedule/adapters/CourseAdapter.java
@@ -1,23 +1,35 @@
 package be.dealloc.schedule.adapters;
 // Created by dealloc. All rights reserved.
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import be.dealloc.schedule.R;
 import be.dealloc.schedule.contracts.entities.courses.Course;
+import be.dealloc.schedule.system.Application;
+import com.amulyakhare.textdrawable.TextDrawable;
 
 import java.util.List;
+
+import static butterknife.ButterKnife.findById;
 
 public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder>
 {
 	private List<Course> courses;
+	private final Drawable DRAWABLE_PRACTICAL;
+	private final Drawable DRAWABLE_THEORETICAL;
+	private final Drawable DRAWABLE_OTHER;
 
 	public CourseAdapter(List<Course> courses)
 	{
 		this.courses = courses;
+		DRAWABLE_PRACTICAL = TextDrawable.builder().buildRound(Application.string(R.string.practical_letter), Application.color(R.color.primary_dark));
+		DRAWABLE_THEORETICAL = TextDrawable.builder().buildRound(Application.string(R.string.theoretical_letter), Application.color(R.color.primary_light));
+		DRAWABLE_OTHER = TextDrawable.builder().buildRound(Application.string(R.string.other_letter), Application.color(R.color.accent));
 	}
 
 	@Override
@@ -31,7 +43,17 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
 	@Override
 	public void onBindViewHolder(CourseViewHolder holder, int position)
 	{
-		holder.setName(this.courses.get(position).getName());
+		Course course = this.courses.get(position);
+		holder.setName(course.getName());
+		holder.setTeachers(course.getTeacher());
+		holder.setLocation(course.getLocation());
+
+		if (course.getType() == Course.PRACTICAL)
+			holder.setImage(DRAWABLE_PRACTICAL);
+		else if (course.getType() == Course.THEORETICAL)
+			holder.setImage(DRAWABLE_THEORETICAL);
+		else
+			holder.setImage(DRAWABLE_OTHER);
 	}
 
 	@Override
@@ -42,17 +64,39 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
 
 	class CourseViewHolder extends RecyclerView.ViewHolder
 	{
+		private ImageView imgType;
 		private TextView txtName;
+		private TextView txtTeachers;
+		private TextView txtLocation;
 
 		public CourseViewHolder(View view)
 		{
 			super(view);
-			this.txtName = (TextView) view.findViewById(R.id.list_txtName);
+			this.imgType = findById(view, R.id.list_imgType);
+			this.txtName = findById(view, R.id.list_txtName);
+			this.txtTeachers = findById(view, R.id.list_txtTeachers);
+			this.txtLocation = findById(view, R.id.list_txtLocation);
+		}
+
+		public void setImage(Drawable image)
+		{
+			this.imgType.setImageDrawable(image);
 		}
 
 		public void setName(String name)
 		{
 			this.txtName.setText(name);
+		}
+
+		public void setTeachers(String teacher)
+		{
+			// TODO If length >= 40 we should add '...' to prevent overflow!
+			this.txtTeachers.setText(teacher);
+		}
+
+		public void setLocation(String location)
+		{
+			this.txtLocation.setText(location);
 		}
 	}
 }
